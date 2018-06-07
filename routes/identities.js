5b88f206fe32b56a1d18e32b47fb85aa67cf70e7
toukucha <chat@oregonstate.edu>
2018-06-07 02:02:46 -0700

    Skeleton code up for Identities

--- routes/identities.js
+++ routes/identities.js
@@ -1,3 +1,34 @@
 const router = require('express').Router();
+const ObjectId = require('mongodb').ObjectId;
+
 
 exports.router = router;
+
+// GET identities/
+router.get('/', function (req, res) {
+
+});
+
+// POST identities/
+router.post('/', function (req, res) {
+
+
+});
+
+// GET identities/:id
+router.get('/:id', function (req, res) {
+
+
+});
+
+// PUT identities/:id
+router.put('/:id', function (req, res) {
+
+
+});
+
+// DELETE identities/:id
+router.delete('/:id', function (req, res) {
+
+
+});
